5709796532b06c51939929870f7ee1f489ca1af1
Conrad Stoerker <conradstoerker@gmail.com>
2015-11-29 00:23:35 -0500

    restriction on now

--- robosoccer-master/soccerpy/midfield_agent.py
+++ robosoccer-master/soccerpy/midfield_agent.py
@@ -179,7 +179,7 @@ class MidfieldAgent (Agent):
 			return True
 		else:
 		    # move towards ball
-		    if -7 <= self.wm.ball.direction <= 7:# and self.wm.euclidean_distance(self.wm.abs_coords,self.goal_pos) <= 60:
+		    if -7 <= self.wm.ball.direction <= 7 and self.wm.euclidean_distance(self.wm.abs_coords,self.goal_pos) <= 75 and self.wm.euclidean_distance(self.wm.abs_coords,self.goal_pos) >= 47 and self.wm.ball.distance < 20:
 
 			self.wm.ah.dash(65)
 			return True
